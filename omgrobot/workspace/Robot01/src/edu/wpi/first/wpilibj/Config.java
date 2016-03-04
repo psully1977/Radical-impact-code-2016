4b6954cafc4e0ff3295da58926901cf1302a6bf8
vipulperiwal <vipulperiwal@Vipul.local>
2016-03-04 15:04:55 -0500

    test
    test

---
+++ omgrobot/workspace/Robot01/src/edu/wpi/first/wpilibj/Config.java
@@ -0,0 +1,23 @@
+package edu.wpi.first.wpilibj;
+
+
+public class Config
+{
+    public class Controller
+    {
+    	public static final int chn = 0;
+    	public static final int maxButtons = 12;
+    	public static final double linearity = 1.5;
+    }
+    
+    public class SwitchCamera
+    {
+   /* 	public static final int btCamCenter = 11;
+    	public static final int btCamRight = 11;
+    */	
+    	
+    	public static final int imgQuality = 60;
+    	public static final String camNameFront = "cam0";
+    	public static final String camNameBack = "cam1";
+    }
+}
